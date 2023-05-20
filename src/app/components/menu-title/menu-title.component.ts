551872a122fa1486ae35b7bf39699bbaf76f3bc0
Vitor Henrique Jorge <vitorhenrique091@outlook.com>
2023-05-19 22:04:00 -0300

    Desacoplando os componentes da parte visual

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: ['./menu-title.component.css']
+  
 })
 export class MenuTitleComponent {
 
